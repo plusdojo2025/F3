eca6960e61a597c5dcc8aa1002972d5e7558a9c6
taku0514seplusdojo <goto-takuto-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 16:54:26 +0900

    Icon,IconStatusDTO作成

--- source/src/main/java/dto/IconStatus.java
+++ source/src/main/java/dto/IconStatus.java
@@ -1,5 +1,26 @@
 package dto;
-
-public class IconStatus {
-
+import java.io.Serializable;
+public class IconStatus implements Serializable{
+	//フィールド
+	private int user_id;
+	private int icon_id;
+	//コンストラクタ
+	public IconStatus(int user_id,int icon_id) {
+		setUser_id(user_id);
+		setIcon_id(icon_id);
+	}
+	//ゲッターセッター
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getIcon_id() {
+		return icon_id;
+	}
+	public void setIcon_id(int icon_id) {
+		this.icon_id = icon_id;
+	}
+	
 }
