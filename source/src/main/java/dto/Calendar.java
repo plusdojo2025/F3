b68fce245ef9f213afccb1da855db922a6bd2e6e
plusdojo2025-ohsaka <ohsaka-yuko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 16:45:21 +0900

    dto

--- source/src/main/java/dto/Calendar.java
+++ source/src/main/java/dto/Calendar.java
@@ -1,5 +1,29 @@
 package dto;
 
-public class Calendar {
+import java.io.Serializable;
 
+public class Calendar implements Serializable{
+private int user_id;
+private String date;
+public Calendar(int user_id, String date) {
+	super();
+	this.user_id = user_id;
+	this.date = date;
+}
+public Calendar() {
+	this.user_id = 0;
+	this.date = "";
+}
+public int getUser_id() {
+	return user_id;
+}
+public void setUser_id(int user_id) {
+	this.user_id = user_id;
+}
+public String getDate() {
+	return date;
+}
+public void setDate(String date) {
+	this.date = date;
+}
 }
