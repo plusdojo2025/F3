a38f610288083de88f2bbb61d3f30c9b8b73938e
plusdojo2025-OishiMasanari <214451370+plusdojo2025-OishiMasanari@users.noreply.github.com>
2025-06-13 16:21:24 +0900

    Garbage
    Garbage

--- source/src/main/java/dto/Garbage_type.java
+++ source/src/main/java/dto/Garbage_type.java
@@ -1,12 +1,13 @@
 package dto;
 import java.io.Serializable;
+import java.sql.Date;
 
 public class Garbage_type implements Serializable{
 	private int region_id;
-	private String region_date;
+	private Date region_date;
 	private String types;
 	
-	public Garbage_type(int region_id, String region_date, String types) {
+	public Garbage_type(int region_id, Date region_date, String types) {
 		super();
 		this.region_id = region_id;
 		this.region_date = region_date;
@@ -16,7 +17,7 @@ public class Garbage_type implements Serializable{
 	public Garbage_type() {
 		super();
 		this.region_id = 0;
-		this.region_date = "";
+		this.region_date = new Date(0);  // 1970-01-01 00:00:00 を初期値に設定
 		this.types = "";
 	}
 
@@ -28,11 +29,11 @@ public class Garbage_type implements Serializable{
 		this.region_id = region_id;
 	}
 
-	public String getRegion_date() {
+	public Date getRegion_date() {
 		return region_date;
 	}
 
-	public void setRegion_date(String region_date) {
+	public void setRegion_date(Date region_date) {
 		this.region_date = region_date;
 	}
 
@@ -43,6 +44,8 @@ public class Garbage_type implements Serializable{
 	public void setTypes(String types) {
 		this.types = types;
 	}
+
+	
 	
 }
 
