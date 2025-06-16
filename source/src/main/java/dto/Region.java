f65214ecee03856a4d97438313f35f730d17a32b
plusdojo2025-ohsaka <ohsaka-yuko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 16:28:38 +0900

    totyuu

--- source/src/main/java/dto/Region.java
+++ source/src/main/java/dto/Region.java
@@ -4,26 +4,26 @@ import java.io.Serializable;
 
 // EmployeeテーブルのDTO（JavaBeans形式）
 public class Region implements Serializable {
-	private int user_id;
+	private int region_id;
 	private String region_name;
 	private String link;
-	public Region(int user_id, String region_name, String link) {
+	public Region(int region_id, String region_name, String link) {
 		super();
-		this.user_id = user_id;
+		this.region_id = region_id;
 		this.region_name = region_name;
 		this.link = link;
 	}
 	public Region() {
 		super();
-		this.user_id = 0;
+		this.region_id = 0;
 		this.region_name = "";
 		this.link = "";
 	}
-	public int getUser_id() {
-		return user_id;
+	public int getRegion_id() {
+		return region_id;
 	}
-	public void setUser_id(int user_id) {
-		this.user_id = user_id;
+	public void setRegion_id(int region_id) {
+		this.region_id = region_id;
 	}
 	public String getRegion_name() {
 		return region_name;
---
+++ source/src/main/webapp/WEB-INF/jsp/top.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ポイポイ｜トップ</title>
+</head>
+<body>
+<button onclick="location.href='./index.html'">ログイン</button>
+<button onclick="location.href='./index.html'">新規登録</button>
+</body>
+</html>
