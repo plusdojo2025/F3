14695f3904dc373b6c5a870796d58c3263fcc961
plusdojo2025-Izumi <izumi-koki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 15:49:56 +0900

    15:49

--- source/src/main/java/servlet/HelpServlet.java
+++ source/src/main/java/servlet/HelpServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.HelpJoinDAO;
+import dto.Region;
 
 /**
  * Servlet implementation class HelpServlet
@@ -21,6 +25,13 @@ public class HelpServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+<<<<<<< Updated upstream
+=======
+		HelpJoinDAO hDAO = new HelpJoinDAO();
+		List<Region> region = hDAO.getGarbageLink(1);
+		request.setAttribute("region", region);
+		System.out.println(region);
+>>>>>>> Stashed changes
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/help.jsp");
 		dispatcher.forward(request, response);
 	}
