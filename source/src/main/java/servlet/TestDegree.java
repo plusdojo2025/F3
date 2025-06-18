f4fa35a7a5542276d2462a740d3ea70251031b3a
plusdojo2025-TUMURBAATAR <tumurbaatar-byambasuren-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 18:09:05 +0900

    06-18トゥムル

--- source/src/main/java/servlet/TestDegree.java
+++ source/src/main/java/servlet/TestDegree.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.time.LocalTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,9 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import dao.UsersDAO;
 
 @WebServlet("/TestDegree")
 public class TestDegree extends HttpServlet {
@@ -18,20 +16,12 @@ public class TestDegree extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-    	HttpSession session = request.getSession();
-    	Object obj = session.getAttribute("id");
-    	int userId = (Integer) obj;
-        System.out.println("user_id="+userId);
-        //セッション送る
-    	session.setAttribute("id", userId); // userId は int 型
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+        System.out.println(hour);
     	
-    	UsersDAO dao1 = new UsersDAO();
-    	boolean inseDeg = dao1.UpDegree(userId);
-    	if(inseDeg) {
-    		System.out.println("称号アップ");
-    	}
-    	
-    	RequestDispatcher dispatcher = request.getRequestDispatcher("/CalendarServlet");
+        request.setAttribute("hour", hour);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/CalendarServlet");
         dispatcher.forward(request, response);
     }
 }
